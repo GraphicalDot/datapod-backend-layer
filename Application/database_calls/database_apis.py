b4bfa7d28073b64229a4b1ee976bcb4ab083c7b7
Kaali <saurav.verma@outlook.com>
2019-04-29 15:53:03 +0530

    images and stats api completed

--- Application/database_calls/database_apis.py
+++ Application/database_calls/database_apis.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*- 
 
 
-
+import subprocess
 import asyncio
 import aiohttp
 from sanic import Blueprint
@@ -56,6 +56,59 @@ async def get_logs(request):
 
 
     
+    return response.json(
+        {
+        'error': False,
+        'success': True,
+        "data": stored_value
+        })
+
+
+@DATABASE_BP.get('/stats')
+async def get_all_stats(request):
+    """
+    To get all the assets created by the requester
+    """
+    def du(path):
+        """disk usage in human readable format (e.g. '2,1GB')"""
+        return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
+
+    number_of_files = sum([len(files) for r, d, files in os.walk(request.app.config.user_data_path)])
+    total_size = du(request.app.config.user_data_path)
+    services = [
+            {"service": "gmail",  "username": "houier.saurav@gmail.com", "last_updated": "2019-04-29 15:49:56 IST+0530"},
+            {"service": "instagram",  "username": "sauravverma86", "last_updated": "2019-04-28 12:12:56 IST+0530"}
+
+    ]
+
+
+    return response.json(
+        {
+        'error': False,
+        'success': True,
+        "data": {"services": services, "files": number_of_files, "size": total_size}
+        })
+
+
+@DATABASE_BP.get('/images/instagram')
+async def get_insta_images(request):
+    """
+    To get all the assets created by the requester
+    """
+    db_instance = create_db_instance(request.app.config.db_dir_path)
+    stored_value = get_key("instagram_images", db_instance)
+
+    if not stored_value:
+        stored_value = []
+    close_db_instance(db_instance)
+
+    logger.info(stored_value)
+    result = []
+    for image in stored_value:
+        file_path = f"file:/{image['path']}"
+        image.update({"file_path": file_path})
+
+
     return response.json(
         {
         'error': False,
