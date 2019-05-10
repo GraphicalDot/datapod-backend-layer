5130434a4722091b64a32d6d4b213d2e0f97ad57
feynman <saurav.verma@outlook.com>
2019-05-11 03:23:06 +0530

    Location is now working but whole data is not Json serializable

--- Application/data_sources/emails/gmail_ds/location.py
+++ Application/data_sources/emails/gmail_ds/location.py
@@ -46,32 +46,27 @@ class Location(object):
         self.activity = activity
 
         self.timestamp, self.year, self.month = month_aware_time_stamp(self.time_stamp)
-        self.hash = self.hashed()
+        self.hash = self.__hash__()
 
     def data(self):
         return self.__dict__
 
-    def hashed(self):
-        string = str(self.latitude)+str(self.longitude)
-        return hashlib.sha3_224(string.encode()).hexdigest()
-
     ##treat places similar if three places decimal matches
     def __hash__(self):
-        latitude = str(self.latitude)[0:6] 
-        longitude = str(self.longitude)[0:6] 
-        string = latitude+longitude 
-        return hash(string.encode())
+        latitude = str(self.latitude)[0:5] 
+        longitude = str(self.longitude)[0:5] 
+        return hash((latitude, longitude, self.year, self.month)) & ((1<<64)-1)
 
 
     def __eq__(self, other):
         """Override the default Equals behavior"""
         if isinstance(other, self.__class__):
-            return self.hash == other.hash and self.month == other.month and self.year == other.year
+            return self.hash == other.hash 
         return False
 
     def __ne__(self, other):
         """Override the default Unequal behavior"""
-        return self.hash != other.hash or self.month != other.month or self.year != other.year
+        return self.hash != other.hash 
 
 class LocationHistory(object):
     
@@ -103,7 +98,6 @@ class LocationHistory(object):
 
 
     def format(self):
-        #self.db_instance = create_db_instance(self.db_dir_path)
         with open(self.path, "rb") as fi:
             result  = fi.read() 
             location = json.loads(result)
@@ -131,81 +125,50 @@ class LocationHistory(object):
                     #month is present
                     self.location_db_data[i_tem.year][i_tem.month] += [i_tem]
 
+        self.store()
+        self.insert_all_data()
+        return 
+
+
+    def store(self):
+        db_instance = create_db_instance(self.db_dir_path)
         for year in self.location_db_data.keys():
             for month in self.location_db_data[year].keys():
-                res = self.location_db_data[year][month]    
+                ##list of the the location object for a month in a year
+                res = self.location_db_data[year][month] 
+
+                key_no_dups = f"location_no_dups_{year}_{month}"
+                key_most_visited = f"location_most_visited_{year}_{month}"
+                most_visited = self.most_visited_places(res)
+                
+                ##set will remove the duplicate elements from the res, duplicate locations
+
+                no_dups = set(res)
                 logger.info(f"Year {year} and Month {month}, number of locations visited is {len(res)} ")
-        #insert_key(self.main_key, self.location_db_data, self.db_instance)
-        #close_db_instance(self.db_instance)
+                logger.info(f"Year {year} and Month {month}, number of non duplicate locations visited is {len(no_dups)} ")
 
-        print (self.most_visited_places(self.location_db_data))
+                insert_key(key_no_dups, [i.data() for i in no_dups],db_instance)
+
+                ##insert the most_visited places after reverse geocoding
+                insert_key(key_most_visited, most_visited,db_instance)
+
+        close_db_instance(db_instance)
         return 
-
 
     def most_visited_places(self, data):
-        most_visited = {}
-        for year in data.keys():
-            for month in data[year].keys():
-                res = data[year][month]
-                
-                ##will make a dictionary out of the list with values as their number
-                ##of duplicacy
-                duplicates = Counter(res)
-                _most_common =  [i[0].data() for i in duplicates.most_common(3)]
-                logger.info(_most_common)
-                for item in _most_common:
-                    address = self.reverse_geo_code(item["latitude"], item["longitude"])
-                    item.update({"address": address})
-
-                if most_visited.get(year):
-                    most_visited[year].update({month: _most_common})
-                else:
-                    most_visited.update({year: {month: _most_common}})
+        duplicates = Counter(data)
+        _most_common =  [i[0].data() for i in duplicates.most_common(5)]
+        for item in _most_common:
+            address = self.reverse_geo_code(item["latitude"], item["longitude"])
+            item.update({"address": address})
+        return _most_common 
 
 
-        return most_visited        
-
-
-    def push_db(self, data, month, year):
+    def insert_all_data(self):
         """
-        {'timestampMs': '1510211478168', 'latitudeE7': 285594542, 'longitudeE7': 772102843, 'accuracy': 1414, 'altitude': 78,
-        'verticalAccuracy': 192, 'timestamp': '2017-11-09 12:41:18 IST+0530', 'year': 2017, 'month': 11}
+        To store all the data on the basis of the year and the month 
         """
-        if not self.location_db_data.get(year):
-            self.location_db_data[year] = [month]
-        else:
-            if month not in self.location_db_data[year]:
-                months = self.location_db_data[year]
-                months.append(month)
-                self.location_db_data[year] = months
-
-        key = f'location_{year}_{month}'
-        logging.info(key)
-        stored_value = get_key(key, self.db_instance)
-
-        value = [data]
-        if stored_value:
-            value = value+stored_value  
-
-
-        insert_key(key, value, self.db_instance)
-        return 
-
-    async def parse(self):
-
-
-
-        tasks = [asyncio.get_event_loop().run_in_executor(
-                                self.th_executor, 
-                                self.reverse_geo_code, 
-                                loc["latitudeE7"]/10000000, 
-                                loc["longitudeE7"]/10000000) for loc in location_data[0:1000]]
-        #several_futures = asyncio.gather(*tasks)
-        #results = loop.run_until_complete(several_futures)
-        completed, pending = await asyncio.wait(tasks)
-        results = [t.result() for t in completed]
-        #results = await asyncio.gather(*tasks)
-        #await asyncio.wait(tasks)
-        #loop.run_until_complete(asyncio.wait(tasks))
-        return results 
-
+        db_instance = create_db_instance(self.db_dir_path)
+        insert_key("location_data", self.location_db_data, db_instance)
+        close_db_instance(db_instance)
+        
