013961808d4859a9729d46cd0fd52a58c413d30c
feynman <saurav.verma@outlook.com>
2019-06-21 01:52:25 +0530

    automatic user update tokens on id_token expiration

--- Application/users_module/login.py
+++ Application/users_module/login.py
@@ -22,7 +22,7 @@ USERS_BP = Blueprint("user", url_prefix="/user")
 
 def revoke_time_stamp(days=0, hours=0, minutes=0, timezone=None): 
     if not timezone:
-        logging.error("Please specify valid timezone for your servers")
+        logger.error("Please specify valid timezone for your servers")
         raise APIBadRequest("Please specify valid timezone for your servers")
     tz_kolkata = pytz.timezone(timezone) 
     time_format = "%Y-%m-%d %H:%M:%S" 
@@ -45,48 +45,80 @@ def id_token_validity():
             #is_authorized = check_request_for_authorization_status(request)
 
             result = get_credentials(request.app.config.CREDENTIALS_TBL)
-            logging.info(result)
+            logger.info(f"Data from the credential table in id_token_validity decorator {result}")
             if not result:
                 logger.error("Credentials aren't present, Please Login again")
                 raise APIBadRequest("Credentials aren't present, Please Login again")
 
 
-            ##TODO Check validity of id_token before 
-            id_token = result["id_token"].decode()
+
+
+            try:
+                id_token = result["id_token"].decode()
+                refresh_token = result["refresh_token"].decode()
+                username = result["username"]
+            except Exception as e:
+                
+                logger.error("User must have signed out, Please Login again")
+                raise APIBadRequest("Please Login again")
+
+
             payload = jwt.get_unverified_claims(id_token)
 
             time_now = datetime.datetime.fromtimestamp(revoke_time_stamp(timezone=request.app.config.TIMEZONE))
             time_expiry = datetime.datetime.fromtimestamp(payload["exp"])
             rd = dateutil.relativedelta.relativedelta (time_expiry, time_now)
 
-
-            print (f"{rd.years} years, {rd.months} months, {rd.days} days, {rd.hours} hours, {rd.minutes} minutes and {rd.seconds} seconds")
+            logger.warning("Difference between now and expiry of id_token")
+            logger.warning(f"{rd.years} years, {rd.months} months, {rd.days} days, {rd.hours} hours, {rd.minutes} minutes and {rd.seconds} seconds")
 
             if rd.minutes < 20:
-                logging.error("Renewing id_token, as it will expire soon")
-                id_token = update_tokens(request)
+                logger.error("Renewing id_token, as it will expire soon")
+                id_token = update_tokens(request, username, refresh_token)
+          
+            if isinstance(id_token, bytes):
+                id_token = id_token.decode()
 
-            # if payload["exp"] < :
-            #     logging.error("Users tokens have expired updating the new tokens")
-            #     update_tokens(request)
-
-            response = await f(request, id_token.decode(), *args, **kwargs)
+            response = await f(request, id_token, username, *args, **kwargs)
             return response
-            # if is_authorized:
-            #     # the user is authorized.
-            #     # run the handler method and return the response
-            #     response = await f(request, *args, **kwargs)
-            #     return response
-            # else:
-            #     # the user is not authorized. 
-            #     return json({'status': 'not_authorized'}, 403)
+          
         return decorated_function
     return decorator
 
 
+
+
+def username():
+    def decorator(f):
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+            # run some method that checks the request
+            # for the client's authorization status
+            #is_authorized = check_request_for_authorization_status(request)
+
+            result = get_credentials(request.app.config.CREDENTIALS_TBL)
+            logger.info(f"Data from the credential table in id_token_validity decorator {result}")
+            if not result:
+                logger.error("Credentials aren't present, Please Login again")
+                raise APIBadRequest("Credentials aren't present, Please Login again")
+
+
+            username = result["username"].decode()
+            response = await f(request,  username, *args, **kwargs)
+            return response
+          
+        return decorated_function
+    return decorator
+
+
+
+
+
+
+
 @USERS_BP.post('/temp_credentials')
 @id_token_validity()
-async def temp_credentials(request, id_token):
+async def temp_credentials(request, id_token, username):
     """
     session is the session which you will get after enabling MFA and calling login api
     code is the code generated from the MFA device
@@ -94,13 +126,11 @@ async def temp_credentials(request, id_token):
     """
 
     
-    # r = requests.post(request.app.config.AWS_CREDS, data=json.dumps({"id_token": id_token}))
-    # result = r.json()
-    # logging.info(result)
-
-    # if result.get("error"):
-    #     logger.error(result["message"])
-    #     raise APIBadRequest(result["message"])
+    r = requests.post(request.app.config.AWS_CREDS, data=json.dumps({"id_token": id_token}))
+    result = r.json()
+    if result.get("error"):
+        logger.error(result["message"])
+        raise APIBadRequest(result["message"])
     
 
     return response.json({
@@ -118,7 +148,7 @@ async def login(request):
 
     r = requests.post(request.app.config.LOGIN, data=json.dumps({"username": request.json["username"], "password": request.json["password"]}))
     result = r.json()
-    logging.info(result)
+    logger.info(result)
     if result.get("error"):
         logger.error(result["message"])
         raise APIBadRequest(result["message"])
@@ -140,24 +170,30 @@ async def login(request):
 
 
 
-
-def update_tokens(request):
-    result = get_credentials(request.app.config.CREDENTIALS_TBL)
-    logging.info(result)
-    if not result:
-        logger.error("Credentials aren't present, Please Login again")
-    
-    r = requests.post(request.app.config.RENEW_REFRESH_TOKEN, data=json.dumps({"username": result["username"], "refresh_token": result["refresh_token"]}))
+def update_tokens(request, username, refresh_token):
+    logger.warning("Updating tokens for the user with the help of refresh token")
+    # result = get_credentials(request.app.config.CREDENTIALS_TBL)
+    # logger.info(result)
+    # if not result:
+    #     logger.error("Credentials aren't present, Please Login again")
+    logger.info(username)
+    logger.info(refresh_token)
+        
+    r = requests.post(request.app.config.RENEW_REFRESH_TOKEN, data=json.dumps({"username": username, "refresh_token": refresh_token}))
+    logging.info(r.text)
     result = r.json()
-    logging.info(result)
+    logger.info(result)
+
     if result.get("error"):
         logger.error(result["message"])
-        raise APIBadRequest(result["message"])
+        raise APIBadRequest("Please login again")
     
 
-    store_credentials(request.app.config.CREDENTIALS_TBL, request.json["username"], 
-                request.json["password"], result["data"]["id_token"], 
-                result["data"]["access_token"], result["data"]["refresh_token"])
+    ##Updating credentials table of the user
+    store_credentials(request.app.config.CREDENTIALS_TBL, username, 
+               result["data"]["id_token"], 
+                result["data"]["access_token"], refresh_token)
+    
     logger.success("tokens are renewed")
     return result["data"]["id_token"]
 
@@ -175,7 +211,7 @@ async def signup(request):
                 "name": request.json["name"],
                 "username": request.json["username"]}))
     result = r.json()
-    logging.info(result)
+    logger.info(result)
 
     if result.get("error"):
         logger.error(result["message"])
@@ -240,9 +276,9 @@ async def aws_temp_creds(request):
 
 
 @USERS_BP.get('/forgot_password')
-async def forgot_password(request):
-    request.app.config.VALIDATE_FIELDS(["email"], request.json)
-    r = requests.post(request.app.config.FORGOT_PASS, data=json.dumps({"email": request.json["email"]}))
+@username()
+async def forgot_password(request, username):
+    r = requests.post(request.app.config.FORGOT_PASS, data=json.dumps({"username": username}))
     result = r.json()
     if result.get("error"):
         logger.error(result["message"])
@@ -259,11 +295,12 @@ async def forgot_password(request):
 
 
 @USERS_BP.get('/new_password')
+@username()
 async def set_new_password(request):
-    request.app.config.VALIDATE_FIELDS(["email", "new_password", "validation_code"], request.json)
+    request.app.config.VALIDATE_FIELDS(["new_password", "validation_code"], request.json)
 
     r = requests.post(request.app.config.CONFIRM_FORGOT_PASS, data=json.dumps({
-                "email": request.json["email"], 
+                "username": request.json["username"], 
                 "password": request.json["new_password"], 
                 "code":  request.json["validation_code"] 
                 }))
@@ -306,14 +343,15 @@ async def associate_mfa(request):
 
 
 @USERS_BP.post('/verify_mfa')
+@username()
 async def verify_mfa(request):
-    request.app.config.VALIDATE_FIELDS(["session", "username", "code"], request.json)
+    request.app.config.VALIDATE_FIELDS(["session", "code"], request.json)
 
     if type(request.json["enabled"]) != bool:
         raise APIBadRequest("Enabled must be boolean")
 
     r = requests.post(request.app.config.VERIFY_MFA, data=json.dumps({"session": request.json["session"], 
-        "username": request.json["username"], "code": request.json["code"]
+        "username": username, "code": request.json["code"]
     }))
     result = r.json()
 
@@ -330,6 +368,7 @@ async def verify_mfa(request):
 
 
 @USERS_BP.post('/post_login_mfa')
+@username()
 async def post_login_mfa(request):
     """
     session is the session which you will get after enabling MFA and calling login api
@@ -337,13 +376,10 @@ async def post_login_mfa(request):
     username is the username of the user
     """
     
-    request.app.config.VALIDATE_FIELDS(["session", "username", "code"], request.json)
-
-    if type(request.json["enabled"]) != bool:
-        raise APIBadRequest("Enabled must be boolean")
+    request.app.config.VALIDATE_FIELDS(["session", "code"], request.json)
 
     r = requests.post(request.app.config.POST_LOGIN_MFA, data=json.dumps({"session": request.json["session"], 
-        "username": request.json["username"], "code": request.json["code"]
+        "username": username, "code": request.json["code"]
     }))
     result = r.json()
 
@@ -361,7 +397,8 @@ async def post_login_mfa(request):
 
 
 @USERS_BP.post('/mnemonics')
-async def mnemonics(request):
+@id_token_validity()
+async def mnemonics(request, id_token, username):
     
     """
     session is the session which you will get after enabling MFA and calling login api
@@ -369,14 +406,14 @@ async def mnemonics(request):
     username is the username of the user
     """
 
-    request.app.config.VALIDATE_FIELDS(["mnemonic", "key_index", "id_token"], request.json)
+    request.app.config.VALIDATE_FIELDS(["mnemonic", "key_index"], request.json)
 
     if type(request.json["enabled"]) != bool:
         raise APIBadRequest("Enabled must be boolean")
 
 
     r = requests.post(request.app.config.MNEMONIC_KEYS, data=json.dumps({"mnemonic": request.json["mnemonic"], 
-        "key_index": request.json["key_index"]}), headers={"Authorization": request.json["id_token"]})
+        "key_index": request.json["key_index"]}), headers={"Authorization": id_token})
 
     try:
         result = r.json()
@@ -391,7 +428,8 @@ async def mnemonics(request):
 
 
 @USERS_BP.post('/profile')
-async def profile(request):
+@id_token_validity()
+async def profile(request, id_token, username):
     
     """
     session is the session which you will get after enabling MFA and calling login api
@@ -399,15 +437,40 @@ async def profile(request):
     username is the username of the user
     """
 
-    request.app.config.VALIDATE_FIELDS(["username", "id_token"], request.json)
-
-    r = requests.post(request.app.config.PROFILE, data=json.dumps({"username": request.json["username"]}), 
-        headers={"Authorization": request.json["id_token"]})
+    r = requests.post(request.app.config.PROFILE, data=json.dumps({"username": username}), 
+        headers={"Authorization": id_token})
     
     result = r.json()
     if r.json["error"]:
         raise APIBadRequest(result["message"])
 
+    return response.json({
+        'error': False,
+        'success': True,
+        "data": result["data"]
+       })
+
+@USERS_BP.post('/logout')
+@id_token_validity()
+async def logout(request, id_token, username):
+    
+    """
+    session is the session which you will get after enabling MFA and calling login api
+    code is the code generated from the MFA device
+    username is the username of the user
+    """
+
+    r = requests.post(request.app.config.LOGOUT, data=json.dumps({"username": username}), 
+        headers={"Authorization": id_token})
+    
+    result = r.json()
+    if r.json["error"]:
+        raise APIBadRequest(result["message"])
+
+    store_credentials(request.app.config.CREDENTIALS_TBL, username, 
+               result["data"]["id_token"], 
+                "", "")
+    
     return response.json({
         'error': False,
         'success': True,
